9f467617d204580fc76a5c2cd8c084ef68dcde84
Ben Patch <fluffypatch@gmail.com>
2023-06-07 19:03:55 -0600

    renamed contentView

--- sqlitePlayground/MainSqliteView.swift
+++ sqlitePlayground/MainSqliteView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainSqliteView.swift
 //  sqlitePlayground
 //
 //  Created by Benjamin Patch on 6/7/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainSqliteView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +21,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainSqliteView()
     }
 }
--- sqlitePlayground/sqlitePlaygroundApp.swift
+++ sqlitePlayground/sqlitePlaygroundApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct sqlitePlaygroundApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainSqliteView()
         }
     }
 }
