c2838f851c286a1d75221cd6f09680db8b2b5ff6
Ben Patch <fluffypatch@gmail.com>
2023-06-07 18:58:20 -0600

    initial commit

---
+++ sqlitePlayground/sqlitePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  sqlitePlaygroundApp.swift
+//  sqlitePlayground
+//
+//  Created by Benjamin Patch on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct sqlitePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
